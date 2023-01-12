b3f84fee9aa3f80a3e68c9bab3f74e99d7b33dd7
Rachel Padworski <rachelpadworski@gmail.com>
2023-01-12 07:58:20 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Contact />
+      <Projects />
+      <Skills />
+      <Testimonials />
+    </main>
   );
 }
 
